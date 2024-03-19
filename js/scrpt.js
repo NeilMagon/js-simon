1c32baaa2a2df92f43d70ba21c4830c66845d86c
NeilMagon <neil.magon97@gmail.com>
2024-03-19 15:48:58 +0100

    feat: add set timeot and array for user numbers

--- js/scrpt.js
+++ js/scrpt.js
@@ -5,6 +5,9 @@
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e 
 // quali dei numeri da indovinare sono stati individuati.
 
+// Seleziono tramite id il mio div 
+let pcDiv = document.querySelector(`#myNumber`);
+
 // Creo l'array dei miei numeri random
 const pcNumber = [];
 
@@ -15,6 +18,21 @@ for (let i = 0; i < 5; i++) {
         pcNumber.push(randomNumber);
     }
 }
+
+//  Mostro i numeri sul mio html
+pcDiv.innerHTML = `${pcNumber}`;
+
+// Dopo 30 secondi i numeri scompaiono 
+setTimeout(() => {
+    pcDiv.innerHTML = ``;
+}, 30000)
+
+// l'utente deve inserire, uno alla volta, 
+// i numeri che ha visto precedentemente, tramite il prompt().
+let userNumbers = [];
+
+let userNumber = parseInt(prompt("Inserisci i numeri che hai visto"));
+userNumbers.push(userNumber);
 
 // Creo una funziona per generare 5 numeri
 function getRandomNumber(min, max) {
